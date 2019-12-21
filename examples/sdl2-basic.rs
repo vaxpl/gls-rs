3fcdef8b5535eecd24691b8329e738440b72581a
Varphone Wong <varphone@qq.com>
2019-12-21 15:02:44 +0800

    Add new example: sdl2-basic
    Signed-off-by: Varphone Wong <varphone@qq.com>

---
+++ examples/sdl2-basic.rs
@@ -0,0 +1,31 @@
+use glplus as gl;
+use sdl2;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("GlPlus - Basic", 800, 600)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    // Create OpenGL context
+    let _gl_context = window.gl_create_context().unwrap();
+    // Load OpenGL routines
+    gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::ffi::c_void);
+    // Set screen clear color
+    gl::clear_color(0.3, 0.3, 0.5, 1.0);
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+        gl::clear(gl::COLOR_BUFFER_BIT);
+        window.gl_swap_window();
+    }
+}
